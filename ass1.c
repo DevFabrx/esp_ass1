5b9100241edfdb2d4d5061a25fac27856ee070af
devfabrx <devfabrx@gmail.com>
2017-10-30 19:29:12 +0100

    added triplet analysis; seems like everything works now

--- ass1.c
+++ ass1.c
@@ -2,100 +2,205 @@
 #include <limits.h>
 #include <stdlib.h>
 #include <float.h>
+#include <math.h>
 
 #define TRUE 1
 #define FALSE 0
 #define SIDES 3
 #define EXIT_SUCCESS 0
-#define EXIT_ERROR 1
+
+
 
 // Function Prototypes
-int get_number_of_triangles();
-float get_triangle_side_length(char*);
-void get_triangle_data(float*, int);
-void print_triangles(float*, int);
-void sort(float [], int);
-void sort_array(float* [][SIDES], int);
+int getNumberOfTriplets();
+float getTripletSideLength(char *);
+void getTripletData(float (*)[SIDES], int);
+//void printTripletData(float (*)[SIDES], int);
+void sort(float (*)[SIDES], int, int);
+void checkTriplets(float (*)[SIDES], int);
+void checkIfTriangle(float *);
+int checkIfRectangular(float *);
+int checkIfEquilateral(float *);
+int checkIfIsosceles(float *);
+int isEqual(float, float);
 
-void sort_array(float* triangle_data[][SIDES], int
-nr_of_triangles)
+void checkTriplets(float (*triplet_data)[SIDES], int nr_of_triplets)
 {
   int i;
-  for(i=0; i<nr_of_triangles; i++)
+  for(i=0; i<nr_of_triplets; i++)
   {
-    printf("DEBUG: Entering sort_array.\n");
-    sort(triangle_data[i], SIDES);
+    printf("Triplet %d ", i+1);
+    checkIfTriangle(triplet_data[i]);
+  }
+}
+
+int checkIfRectangular(float* triplet_data)
+{
+  float a = triplet_data[0];
+  float b = triplet_data[1];
+  float c = triplet_data[2];
+  if(isEqual(a * a + b * b, c * c))
+  {
+    return TRUE;
+  }
+  else
+  {
+    return FALSE;
+  }
+}
+
+
+int checkIfIsosceles(float *triplet_data)
+{
+  float a = triplet_data[0];
+  float b = triplet_data[1];
+  if(isEqual(a, b))
+  {
+    return TRUE;
+  }
+  else
+  {
+    return FALSE;
+  }
+}
+
+int checkIfEquilateral(float *triplet_data)
+{
+  float a = triplet_data[0];
+  float b = triplet_data[1];
+  float c = triplet_data[2];
+  if(isEqual(a, b) && isEqual(b, c))
+  {
+    return TRUE;
+  }
+  else
+  {
+    return FALSE;
+  }
+}
+
+int isEqual(float a, float b)
+{
+  if(fabs(a-b) >= FLT_EPSILON)
+  {
+    return 0;
+  }
+  else
+  {
+    return 1;
+  }
+}
+
+void checkIfTriangle(float *triplet_data)
+{
+  float a = triplet_data[0];
+  float b = triplet_data[1];
+  float c = triplet_data[2];
+
+  if(isEqual(a + b, c) || fabs(a+b-c)<FLT_EPSILON)
+  {
+    printf("(a=%.6f, b=%.6f, c=%.6f) is NO triangle.\n", a, b, c);
+  }
+  else
+  {
+    printf("(a=%.6f, b=%.6f, c=%.6f) is a triangle.\n", a, b, c);
+    if(checkIfEquilateral(triplet_data))
+    {
+      printf(" It is an equilateral triangle.\n");
+      printf(" It is an isosceles triangle.\n");
+    }
+    else if(checkIfIsosceles(triplet_data))
+    {
+      printf(" It is an isosceles triangle.\n");
+      if(checkIfRectangular(triplet_data))
+      {
+        printf(" It is a right triangle.\n");
+      }
+    }
+    else if(checkIfRectangular(triplet_data))
+    {
+      printf(" It is a right triangle.\n");
+    }
+
   }
 }
 
 
 //-----------------------------------------------------------------------------
 ///
-/// Sorts an float array in increasing order
-/// @param array float pointer to an float array
-/// @param len length of the array
+/// Bubble sorts an 2-d float array in increasing order
+/// @param triplet_data pointer to the triplet array that needs to be sorted
+/// @param  nr_of_triplets number of triplets (= rows) in the 2-d array
+/// @param length length of the array to be sorted
 /// return void
 //
-void sort(float array[], int length)
+void sort(float (*triplet_data)[SIDES], int nr_of_triplets, int length)
 {
-  printf("DEBUG: Entering sort.\n");
+  int k;
   int i;
   int j;
-
-  for(i=0; i < length-1; i++)
+  for(k=0; k<nr_of_triplets; k++)
   {
-    for(j=0; j < length-1; j++)
+    float* data_row = triplet_data[k];
+    for(i=0; i < length-1; i++)
     {
-      if (array[j] > array[j+1])
+      for(j=0; j < length-1; j++)
       {
-        float tmp = array[j];
-        array[j] = array[j+1];
-        array[j+1] = tmp;
+        if (data_row[j] > data_row[j+1])
+        {
+          float tmp = data_row[j];
+          data_row[j] = data_row[j+1];
+          data_row[j+1] = tmp;
+        }
       }
     }
   }
 }
 
-//-----------------------------------------------------------------------------
-///
-/// Prints the currently stored data of the triangles
-/// @param triangle_data pointer to the triangle array
-/// @param size number of triangles in array
-/// return void
-//
-void print_triangles(float* triangle_data, int size)
-{
-  int i;
-  int j=0;
-  for(i=0; i < size; i++)
-  {
-    if(j == 3)
-    {
-      printf("\n");
-      j = 0;
-    }
-    printf("%f ",*(triangle_data+i));
-    j += 1;
-  }
-}
 
 //-----------------------------------------------------------------------------
 ///
-/// Asks the user to input all data for the triangles he specified
-/// @param triangle_data float pointer to the triangle data array
-/// @param nr_of_triangles number of lines of the data array
+/// Prints the currently stored triplet data
+/// @param triplet_data pointer to the triplet array
+/// @param nr_of_triplets number of triangles in array
+/// return void
+//
+void printTripletData(float (*triplet_data)[SIDES], int nr_of_triplets)
+{
+  int i;
+  int j;
+  for(i=0; i < nr_of_triplets; i++)
+  {
+    printf("Triplet %d: ", i+1);
+    for(j=0; j < SIDES; j++)
+    {
+      printf("%f ", triplet_data[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+
+//-----------------------------------------------------------------------------
+///
+/// Asks the user to input all side length data for the number of triangles he
+/// specified
+/// @param triplet_data float pointer to the triangle data array
+/// @param nr_of_triplets number of elements in the data array
 ///
 //
-void get_triangle_data(float* triangle_data, int nr_of_triangles)
+void getTripletData(float (*triplet_data)[SIDES], int nr_of_elements)
 {
   char* sides_array[]= {"first", "second", "third"};
 
   int i;
   int j;
-  for (i=0; i < nr_of_triangles*SIDES; i++)
+  for (i=0; i < nr_of_elements; i++)
   {
-    j = i%3;
-    *(triangle_data+i) = get_triangle_side_length(sides_array[j]);
+    for(j=0; j<SIDES; j++)
+    {
+      triplet_data[i][j] = getTripletSideLength(sides_array[j]);
+    }
   }
 }
 
@@ -108,7 +213,7 @@ void get_triangle_data(float* triangle_data, int nr_of_triangles)
 ///
 /// @return float triangle side length
 //
-float get_triangle_side_length(char* name)
+float getTripletSideLength(char *name)
 {
 
   while(TRUE)
@@ -133,28 +238,27 @@ float get_triangle_side_length(char* name)
 
 //-----------------------------------------------------------------------------
 ///
-/// This function asks for the number of triangles to check and also checks
+/// This function asks for the number of triplets to check and also checks
 /// if the input is correct.
 ///
-/// @return int number of triangles to check
+/// @return int nr_of_triplets
 //
-int get_number_of_triangles()
+int getNumberOfTriplets()
 {
-  printf("DEBUG: Entering get_number_of_triangles.\n");
   while(TRUE)
   {
     char input[] = ""; // input char pointer initialized to null
     printf("Please enter the number of triangles to check: ");
     scanf("%s", input);
-    int nr_of_triangles = atoi(input);
-    if (nr_of_triangles == 0 || nr_of_triangles > UCHAR_MAX )
+    int nr_of_triplets = atoi(input);
+    if (nr_of_triplets == 0 || nr_of_triplets > UCHAR_MAX )
     {
       printf("[ERR] Invalid number of triangles.\n");
       continue;
     }
     else
     {
-      return nr_of_triangles;
+      return nr_of_triplets;
     }
   }
 }
@@ -175,18 +279,17 @@ int get_number_of_triangles()
 //
 int main()
 {
-  int nr_of_triangles = get_number_of_triangles();
-  printf("You want to check %d triplets\n", nr_of_triangles);
-  float triangle_data[nr_of_triangles][SIDES];
-  int nr_of_elements = sizeof(triangle_data)/ sizeof(triangle_data[0][0]);
-  get_triangle_data(triangle_data, nr_of_triangles);
+  int nr_of_triplets = getNumberOfTriplets();
+  float triplet_data[nr_of_triplets][SIDES];
+  //int nr_of_elements = sizeof(triplet_data)/ sizeof(triplet_data[0][0]);
+  getTripletData(triplet_data, nr_of_triplets);
 
-  print_triangles(triangle_data, nr_of_elements);
-  printf("\n________________________________\n\n");
-  sort_array(triangle_data, nr_of_triangles);
+  //printTripletData(triplet_data, nr_of_triplets);
+  //printf("\n________________________________\n");
+  sort(triplet_data, nr_of_triplets, SIDES);
 
-  print_triangles(triangle_data, nr_of_elements);
-
+  //printTripletData(triplet_data, nr_of_triplets);
+  checkTriplets(triplet_data, nr_of_triplets);
 
   return EXIT_SUCCESS;
 }
